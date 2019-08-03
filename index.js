9cae16642a6973c5c3e3de092fef8b090347259e
Connor Meth <connormeth27@gmail.com>
2019-08-03 19:40:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 // write your code below!
-var name = "you"
-var holiday = "Independence Day"
-var days = 20
 
 function happyHolidays(string) {
   return "Happy holidays!";
@@ -13,5 +10,6 @@ function happyHolidayTo(string) {
   return `Happy ${holiday}, ${name}!`;
 }
 function holidayCountdown(string) {
+  var days = 20
   return `It\'s ${days} days until ${holiday}`;
 }
